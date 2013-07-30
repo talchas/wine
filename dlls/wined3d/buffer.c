375d37193f4ea8e57f7844a61c2ff9bb49dfb23e
Stefan Dösinger <stefan@codeweavers.com>
2013-07-30 22:17:10 +0200

    wined3d: Control WINED3D_BUFFER_DISCARD from the worker thread

--- dlls/wined3d/buffer.c
+++ dlls/wined3d/buffer.c
@@ -1004,7 +1004,7 @@ HRESULT CDECL wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UIN
                 context_release(context);
             }
         }
-        else
+        else if(!wined3d_settings.cs_multithreaded)
         {
             if (dirty)
             {
@@ -1334,4 +1334,11 @@ HRESULT CDECL wined3d_buffer_create_ib(struct wined3d_device *device, UINT size,
     *buffer = object;
 
     return WINED3D_OK;
+}
+
+void buffer_swap_mem(struct wined3d_buffer *buffer, BYTE *mem)
+{
+    wined3d_resource_free_sysmem(&buffer->resource);
+    buffer->resource.heap_memory = mem;
+    buffer->flags |= WINED3D_BUFFER_DISCARD;
 }
