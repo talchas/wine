9479e0c11d1e5c1f5c3c5a1ff70d08f361a08137
Stefan Dösinger <stefan@codeweavers.com>
2013-11-17 18:44:48 +0100

    wined3d: Invalidate containers via callback.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -1979,6 +1979,7 @@ static inline void context_invalidate_active_texture(struct wined3d_context *con
 struct wined3d_resource_ops
 {
     void (*resource_unload)(struct wined3d_resource *resource);
+    void (*resource_location_invalidated)(struct wined3d_resource *resource, DWORD location);
 };
 
 struct wined3d_resource
