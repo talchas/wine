516bb520a3d8985e5ba1b5385a772c4a055dd8ed
Stefan Dösinger <stefan@codeweavers.com>
2014-01-04 13:20:25 +0100

    wined3d: Discard implicit surfaces on unload.

--- dlls/wined3d/surface.c
+++ dlls/wined3d/surface.c
@@ -1203,25 +1203,14 @@ static void surface_unload(struct wined3d_resource *resource)
     context = context_acquire(device, NULL);
     gl_info = context->gl_info;
 
-    surface_prepare_system_memory(surface);
     if (resource->pool == WINED3D_POOL_DEFAULT)
     {
         /* Default pool resources are supposed to be destroyed before Reset is called.
          * Implicit resources stay however. So this means we have an implicit render target
          * or depth stencil. The content may be destroyed, but we still have to tear down
-         * opengl resources, so we cannot leave early.
-         *
-         * Put the surfaces into sysmem, and reset the content. The D3D content is undefined,
-         * but we can't set the sysmem INDRAWABLE because when we're rendering the swapchain
-         * or the depth stencil into an FBO the texture or render buffer will be removed
-         * and all flags get lost.
-         *
-         * TODO: Setting WINED3D_LOCATION_DISCARDED is be a better solution, but it only works for
-         * depth stencils so far. */
-
-        if (surface->resource.heap_memory)
-            memset(surface->resource.heap_memory, 0, surface->resource.size);
-        wined3d_resource_validate_location(&surface->resource, WINED3D_LOCATION_SYSMEM);
+         * opengl resources, so we cannot leave early. */
+        wined3d_resource_validate_location(&surface->resource, WINED3D_LOCATION_DISCARDED);
+        wined3d_resource_invalidate_location(&surface->resource, ~WINED3D_LOCATION_DISCARDED);
 
         /* We also get here when the ddraw swapchain is destroyed, for example
          * for a mode switch. In this case this surface won't necessarily be
@@ -1231,9 +1220,10 @@ static void surface_unload(struct wined3d_resource *resource)
     }
     else
     {
+        surface_prepare_system_memory(surface);
         wined3d_resource_load_location(&surface->resource, context, WINED3D_LOCATION_SYSMEM);
+        wined3d_resource_invalidate_location(&surface->resource, ~WINED3D_LOCATION_SYSMEM);
     }
-    wined3d_resource_invalidate_location(&surface->resource, ~WINED3D_LOCATION_SYSMEM);
     surface->flags &= ~(SFLAG_ALLOCATED | SFLAG_SRGBALLOCATED);
 
     /* Destroy fbo render buffers. This is needed for implicit render targets, for
